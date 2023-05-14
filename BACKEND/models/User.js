1d0d0d992acdc06a1dfc07273d80875124f1ad95
Rusiru Herath <it21002106@my.sliit.lk>
2023-05-14 16:10:28 +0530

    Create user models & routes

---
+++ BACKEND/models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255,
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 16,
+    },
+    mobile: {
+        type: String,
+        required: true,
+        min: 10,
+    },
+
+    nic: {
+        type: String,
+        required: true,
+        min: 10,
+    },
+});
+
+module.exports = mongoose.model("User", userSchema);
