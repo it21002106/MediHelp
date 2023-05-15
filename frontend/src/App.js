32ae369b6c4a751f60f0144d7452c3be7738132b
Ranjula001 <it21011016@my.sliit.lk>
2023-05-15 12:05:42 +0530

    changes done in backend and frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,40 @@
 import './App.css';
-import AdminDashbroad from "./Component/AdminDashbroad";
+
 import Allhospital from "./Component/Allhospital";
 import EditHospital from "./Component/Edit";
-import {BrowserRouter as Router, Route , Routes} from "react-router-dom"
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import GenRepo from "./Component/GenHospitalRepo";
 import Addhospital from "./Component/Addhospital";
+import AllProject from "./Component/allProject";
+import AddProject from "./Component/AddProject";
+
+
+import EditProject from "./Component/EditProjects";
+
+import AllDonations from "./Component/AllDonations";
 
 
 
 function App() {
-  return (
-    <Router>  
-         
-      <Routes>
-      
-        <Route path = "/" element = {<AdminDashbroad/>} />
-        <Route path = "/all" element = {<Allhospital/>} />
-        <Route path = "/Update/:id" element = {<EditHospital/>} />
-        <Route path = "/SupReport" element ={<GenRepo/>} />
-        <Route path = "/add" element = {<Addhospital/>} /> 
-       
-        
-        </Routes>     
-</Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                
+                <Route path="/all" element={<Allhospital/>}/>
+                <Route path="/Update/:id" element={<EditHospital/>}/>
+                <Route path="/SupReport" element={<GenRepo/>}/>
+                <Route path="/add" element={<Addhospital/>}/>
+                <Route path="/allProjects" element={<AllProject/>}/>
+                <Route path="/addProjects" element={<AddProject/>}/>
+                
+                
+                
+                <Route path="/UpdateProject/:id" element={<EditProject/>}/>
+                
+                <Route path="/allDonations" element={<AllDonations/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
