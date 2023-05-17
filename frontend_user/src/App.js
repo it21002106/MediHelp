604f96f91c735dfcf7a71a39382840728b558984
Ranjula001 <it21011016@my.sliit.lk>
2023-05-17 12:36:03 +0530

    did some changes to fix errors

--- frontend_user/src/App.js
+++ frontend_user/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import AdminDashbroad from "./Component/AdminDashbroad";
+
 import Allhospital from "./Component/Allhospital";
 import EditHospital from "./Component/Edit";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
@@ -8,31 +8,22 @@ import GenRepo from "./Component/GenHospitalRepo";
 import Addhospital from "./Component/Addhospital";
 import AllProject from "./Component/allProject";
 import AddProject from "./Component/AddProject";
-import AllEvents from "./Component/AllEvents";
-import AddEvent from "./Component/AddEvent";
-import AllPrescriptions from "./Component/AllPrescriptions";
-import AddPrescription from "./Component/AddPrescription";
-import EditEvent from "./Component/EditEvent";
-import EditPrescription from "./Component/EditPrescription";
 import EditProject from "./Component/EditProjects";
 import Login from "./Component/login";
 import Signup from "./Component/signUp";
 import AddDoanation from "./Component/AddDonation";
 import AllDonations from "./Component/AllDonations";
 import Home from "./Component/Home";
-import MyPrescriptions from "./Component/MyPrescriptions";
 
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
-import AdminDashbroad from "./Component/AdminDashbroad";
-import Login from "./Component/login";
-import Signup from "./Component/signUp";
 
-import Allhospital from "./Component/Allhospital";
-import EditHospital from "./Component/Edit";
-import GenRepo from "./Component/GenHospitalRepo";
-import Addhospital from "./Component/Addhospital";
 
-import Home from "./Component/Home";
+
+
+
+
+
+
+
 
 
 
